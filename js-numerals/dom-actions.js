979ef8bd9d833f32d36a392f03da399bbe4b8330
Tbence98 <tlrbence@gmail.com>
2023-01-07 21:59:53 +0100

    add checkbox for toggling British English mode

--- js-numerals/dom-actions.js
+++ js-numerals/dom-actions.js
@@ -1,15 +1,25 @@
 import numbersToLetters from "./numbers-to-letters.js";
+import { convertToBritishEnglish } from "./numbers-to-letters.js";
 
 const form = document.querySelector(".converter-form");
+const isBritishCheckBox = document.getElementById("isBritish");
 
 const submitHandler = (event) => {
     event.preventDefault();
-    const numberEl = document.getElementById("number-input");
     const resultEl = document.getElementById("result");
+    const numberEl = document.getElementById("number-input");
     const numberToConvert = +numberEl.value;
 
     try {
-        resultEl.innerText = numbersToLetters(numberToConvert);
+        if (
+            isBritishCheckBox.checked &&
+            numberToConvert > 1000 &&
+            numberToConvert < 2000
+        ) {
+            resultEl.innerText = convertToBritishEnglish(numberToConvert);
+        } else {
+            resultEl.innerText = numbersToLetters(numberToConvert);
+        }
     } catch (e) {
         resultEl.innerText = e.message;
     }
